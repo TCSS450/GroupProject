0d137fe8e0a6903de7411ebde84b75db04009e53
Josh Atherton <joshua9@uw.edu>
2018-11-30 12:29:14 -0800

    Fixed my view friends not displaying

--- app/src/main/java/group3/tcss450/uw/edu/groupappproject/fragments/ViewFriends/ViewFriends_Main.java
+++ app/src/main/java/group3/tcss450/uw/edu/groupappproject/fragments/ViewFriends/ViewFriends_Main.java
@@ -17,6 +17,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import group3.tcss450.uw.edu.groupappproject.R;
